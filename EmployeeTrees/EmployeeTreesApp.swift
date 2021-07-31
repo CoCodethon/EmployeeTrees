dbada19e157826c703884da0eb45fa8f772e01e4
TubaMirabilis <bkingsleygreen@gmail.com>
2021-07-31 01:56:57 +0100

    Initial Commit

---
+++ EmployeeTrees/EmployeeTreesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmployeeTreesApp.swift
+//  EmployeeTrees
+//
+//  Created by Benjamin Green on 31/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct EmployeeTreesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
