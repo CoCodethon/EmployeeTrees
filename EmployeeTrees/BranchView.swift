1061ad5739068a9c715f24e0aaf602b090351e58
TubaMirabilis <bkingsleygreen@gmail.com>
2021-07-31 06:19:48 +0100

    Added a BranchView and setup some mock data in ContentView

---
+++ EmployeeTrees/BranchView.swift
@@ -0,0 +1,20 @@
+//
+//  BranchView.swift
+//  EmployeeTrees
+//
+//  Created by Benjamin Green on 31/07/2021.
+//
+
+import SwiftUI
+
+struct BranchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BranchView_Previews: PreviewProvider {
+    static var previews: some View {
+        BranchView()
+    }
+}
